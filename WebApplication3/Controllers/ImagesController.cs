84751026838777108824c71f46db9c0cd73708d7
Opeyemi Olorunfemi <oolorunfemi508@gmail.com>
2018-11-08 08:49:08 -0500

    First commit

---
+++ WebApplication3/Controllers/ImagesController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using WebApplication3.Models;
+using WebApplication3.Helpers;
+using System.IO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    public class ImagesController : Controller
+    {
+        private readonly AzureStorageConfig storageConfig = null;
+
+        public ImagesController(IOptions<AzureStorageConfig> config)
+        {
+            storageConfig = config.Value;
+
+        }
+        //Post/api/images/upload
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload(ICollection<IFormFile> files)
+        {
+            bool isUploaded = false;
+            try
+            {
+                if (files.Count == 0)
+                {
+                    return BadRequest("No files received from the upload");
+                }
+                if (storageConfig.ImageContainer == string.Empty)
+                {
+                    return BadRequest("Please provide a name for your image container in the azure blob storage ");
+                }
+                foreach (var formfile in files)
+                {
+                    if (StorageHelpers.IsImage(formfile))
+                    {
+                        if (formfile.Length > 0)
+                        {
+                            using (Stream stream = formfile.OpenReadStream())
+                            {
+                                isUploaded = await StorageHelpers.UploadFileTorStorage(stream, formfile.FileName, storageConfig);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return new UnsupportedMediaTypeResult();
+                    }
+                }
+                if (isUploaded)
+                {
+                    if (storageConfig.ThumbnailContainer != string.Empty)
+                    {
+                        return new AcceptedAtActionResult("GetThumbNails", "Images", null, null);
+                    }
+                    else
+                        return new AcceptedResult();
+                }
+                else
+                    return BadRequest("Look like the image couldn't upload to the storage");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("thumbnails")]
+        public async Task<IActionResult> GetThumbnails()
+        {
+            try
+            {
+                if (storageConfig.AccountKey == string.Empty || storageConfig.AccountName == string.Empty)
+                    return BadRequest("Sorry, can't retrieve your azure storage details from appsetttings.js make sre that you add azure storage details there");
+                if (storageConfig.ImageContainer == string.Empty)
+
+                    return BadRequest("Please provide a name for your image conatiner in the azure blob storage");
+
+                List<string> thumbnails = await StorageHelpers.GetThumnailsUrls(storageConfig);
+                return new ObjectResult(thumbnails);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebApplication3/Helpers/StorageHelpers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
+
+namespace WebApplication3.Helpers
+{
+    public static class StorageHelpers
+    {
+        public static bool IsImage(IFormFile file)
+        {
+            if (file.ContentType.Contains("image"))
+            {
+                return true;
+            }
+            string[] formats = new string[] { ".jpg", ".png", ".gif", ".jpeg" };
+            return formats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
+        }
+        public static async Task<bool> UploadFileTorStorage(Stream fileStream, string fileName, AzureStorageConfig _storageConfig)
+        {
+            StorageCredentials storageCredentials = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+            CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(storageCredentials, true);
+            CloudBlobClient blobClient = cloudStorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ImageContainer);
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            await blockBlob.UploadFromStreamAsync(fileStream);
+            return await Task.FromResult(true);
+        }
+        public static async Task<List<string>> GetThumnailsUrls(AzureStorageConfig _storageConfig)
+        {
+            List<string> thumbnailsUrls = new List<string>();
+            StorageCredentials storageCredentials = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ThumbnailContainer);
+            BlobContinuationToken continuationToken = null;
+            BlobResultSegment resultSegment = null;
+
+            do
+            {
+                resultSegment = await container.ListBlobsSegmentedAsync("", true, BlobListingDetails.All, 10, continuationToken, null, null);
+                foreach (var blobItem in resultSegment.Results)
+                {
+                    thumbnailsUrls.Add(blobItem.StorageUri.PrimaryUri.ToString());
+                }
+                continuationToken = resultSegment.ContinuationToken;
+            }
+            while (continuationToken != null);
+            return await Task.FromResult(thumbnailsUrls);
+
+        }
+    }
+}
